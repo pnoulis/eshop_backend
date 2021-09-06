9a12ff7fdfd6902b0d329a177c41d72df3e34c80
pavlos noulis <pavlos.noulis@gmail.cqm>
2021-09-06 14:08:53 +0300

    allowed servicing more ports in cors

--- config/config.js
+++ config/config.js
@@ -11,7 +11,8 @@ publicDir = "/public",
 serverRoot = "nnbackend",
 cors = {
   // Access-Control-Allow-Origin
-  origin: `${clientDomain}`,
+  origin: [clientDomain, "http://localhost:3002", "http://localhost:3003"],
+  // origin: `${clientDomain}`,
   // Access-Control-Allow-Credentials
   credentials: true,
 },
